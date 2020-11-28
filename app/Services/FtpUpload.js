ee47f386888d2aa27eabfff750cdd77261eae76a
juniorcintra <junior.developer@outlook.com.br>
2020-11-28 02:37:25 -0300

    teste

--- app/Services/FtpUpload.js
+++ app/Services/FtpUpload.js
@@ -15,7 +15,7 @@ class FtpUpload {
 
     const fileName = `${new Date().getTime()}.${file.extname}`;
 
-    const pathFTP = `/${caminho}/${fileName}`;
+    const pathFTP = `/www/${caminho}/${fileName}`;
 
     const client = new Ftp.Client();
     client.ftp.verbose = true;
